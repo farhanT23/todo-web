db2ef504d7c156647ffe4d020e505f9ffdd786f8
farhanT23 <farhan.tanvir@brainstation-23.com>
2025-04-10 19:01:45 +0600

    ui(landing page): complete landing page design and route

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\LandingController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
+Route::get('/', [LandingController::class, 'showGreeting'])->name('landing');;
 
 Route::prefix("auth")->group(function () {
     Route::get('registration', [\App\Http\Controllers\AuthController::class, 'registration'])->name('registration');
