00d033727716b0c447358bb658225f2408d50911
farhanT23 <farhan.tanvir@brainstation-23.com>
2025-04-11 15:00:33 +0600

    feat(login): Complete login authentication

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -10,7 +11,7 @@ class AuthController extends Controller
         return view('auth.registration');
     }
 
-    public function login(){
+    public function showLoginForm(){
         return view('auth.login');
     }
 
@@ -20,5 +21,34 @@ class AuthController extends Controller
 
     public function forgetPassword(){
         return view('auth.forgetPassword');
+    }
+
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            // check is_active
+            if (!Auth::user()->is_active) {
+                Auth::logout();
+                return back()->withErrors(['email' => 'Your account is not active.']);
+            }
+
+            //check if the user is verified
+            if (!Auth::user()->is_verified) {
+                Auth::logout();
+                return back()->withErrors(['email' => 'Your account is not verified.']);
+            }
+
+            return redirect()->intended('/dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,8 @@ Route::get('/', [LandingController::class, 'showGreeting'])->name('landing');;
 
 Route::prefix("auth")->group(function () {
     Route::get('registration', [\App\Http\Controllers\AuthController::class, 'registration'])->name('registration');
-    Route::get('login', [\App\Http\Controllers\AuthController::class, 'login'])->name('login');
+    Route::get('login', [\App\Http\Controllers\AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
     Route::get('new-password', [\App\Http\Controllers\AuthController::class, 'newPassword'])->name('new-password');
     Route::get('forget-password', [\App\Http\Controllers\AuthController::class, 'forgetPassword'])->name('forget-password');
 });
@@ -20,4 +21,5 @@ Route::prefix("auth")->group(function () {
 Route::prefix("dashboard")->group(function () {
     Route::get('/', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
 });
+
 
