4a909a0fccc85c6236d9903c6f122319cce5798a
farhanT23 <farhan.tanvir@brainstation-23.com>
2025-04-11 11:49:16 +0600

    migration(user): Completed migration for users table

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'is_verified',
+        'is_active',
+        'otp',
     ];
 
     /**
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,12 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id()->primary();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->boolean('is_verified');
+            $table->boolean('is_active');
+            $table->string('otp')->nullable();;
             $table->timestamps();
         });
 
