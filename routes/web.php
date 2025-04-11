977f970c7123665a97dca055e50ccdd18100fd0a
farhanT23 <farhan.tanvir@brainstation-23.com>
2025-04-11 11:07:37 +0600

    chore(routing): modify route URI to align with new naming convention

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ Route::get('/', [LandingController::class, 'showGreeting'])->name('landing');;
 Route::prefix("auth")->group(function () {
     Route::get('registration', [\App\Http\Controllers\AuthController::class, 'registration'])->name('registration');
     Route::get('login', [\App\Http\Controllers\AuthController::class, 'login'])->name('login');
-    Route::get('newPassword', [\App\Http\Controllers\AuthController::class, 'newPassword'])->name('newPassword');
-    Route::get('forgetPassword', [\App\Http\Controllers\AuthController::class, 'forgetPassword'])->name('forgetPassword');
+    Route::get('new-password', [\App\Http\Controllers\AuthController::class, 'newPassword'])->name('new-password');
+    Route::get('forget-password', [\App\Http\Controllers\AuthController::class, 'forgetPassword'])->name('forget-password');
 });
 
 Route::prefix("dashboard")->group(function () {
