6a636d1f2acc88ccf3e067d119104051805a92ed
Aslam Mahmud Siam <aslam.mahmud@brainstation-23.com>
2025-04-11 14:35:57 +0600

    feature(registration): registration with verify email send

--- resources/views/auth/registration.blade.php
+++ resources/views/auth/registration.blade.php
@@ -5,8 +5,8 @@
 
         <h1 class="text-2xl font-bold text-center mb-5">Register</h1>
 
-        <form class="w-full">
-
+        <form method="post" class="w-full">
+            @csrf
             <x-input name="name" label="Your Name" type="name" />
             <x-input name="email" label="Your email" type="email" />
             <x-input name="password" label="Your password" type="password" />
@@ -15,14 +15,12 @@
             <x-button type="submit" class="w-full mt-5">
                 Register
             </x-button>
-
-            @if (Route::has('login'))
-                <p class="text-sm font-light text-gray-500 mt-5 text-center">
-                    Already have an account? <a href="{{ route('login') }}"
-                        class="font-medium text-blue-600 hover:underline">Login</a>
-                </p>
-            @endif
-
         </form>
+        @if (Route::has('login'))
+            <p class="text-sm font-light text-gray-500 mt-5 text-center">
+                Already have an account? <a href="{{ route('login') }}"
+                    class="font-medium text-blue-600 hover:underline">Login</a>
+            </p>
+        @endif
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,12 @@ Route::get('/', [LandingController::class, 'showGreeting'])->name('landing');;
 
 
 Route::prefix("auth")->group(function () {
+
     Route::get('registration', [\App\Http\Controllers\AuthController::class, 'registration'])->name('registration');
+    Route::post('registration', [\App\Http\Controllers\AuthController::class, 'registrationSave'])->name('registration-save');
+
+    Route::get('verify-email/{otp}', [\App\Http\Controllers\AuthController::class, 'verifyEmail'])->name('verify');
+
     Route::get('login', [\App\Http\Controllers\AuthController::class, 'login'])->name('login');
     Route::get('new-password', [\App\Http\Controllers\AuthController::class, 'newPassword'])->name('new-password');
     Route::get('forget-password', [\App\Http\Controllers\AuthController::class, 'forgetPassword'])->name('forget-password');
@@ -20,4 +25,3 @@ Route::prefix("auth")->group(function () {
 Route::prefix("dashboard")->group(function () {
     Route::get('/', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
 });
-
